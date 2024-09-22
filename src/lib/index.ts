41a3edb847f4e6b7ae74ecbdafef8f37e11068b1
Nate <nrobison1509@gmail.com>
2024-09-21 21:04:13 -0700

    Forgot to build the package before publishing

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -3,3 +3,8 @@ import './index.css';
 export { default as AlertDialog } from './components/AlertDialog.svelte';
 export { default as Button } from './components/Button.svelte';
 export { default as Input } from './components/Input.svelte';
+export { default as Heading } from './components/Heading.svelte';
+export { default as Grid } from './components/Grid/Grid.svelte';
+export { default as GridItem } from './components/Grid/GridItem.svelte';
+export { default as Navbar } from './components/Navbar/Navbar.svelte';
+export { default as NavbarItem } from './components/Navbar/NavbarItem.svelte';
