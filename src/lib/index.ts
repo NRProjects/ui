724dbd1e65001101046008b3267fe7571ea5919f
Nate <nrobison1509@gmail.com>
2024-11-16 17:46:33 -0800

    Updated exports

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -7,7 +7,4 @@ export { default as Input } from './components/Input.svelte';
 export { default as Heading } from './components/Heading.svelte';
 export { default as Grid } from './components/Grid/Grid.svelte';
 export { default as GridItem } from './components/Grid/GridItem.svelte';
-export { default as Navbar } from './components/Navbar/Navbar.svelte';
-export { default as NavbarItem } from './components/Navbar/NavbarItem.svelte';
 export { default as Label } from './components/Label.svelte';
-export { default as Select } from './components/Select/Select.svelte';
